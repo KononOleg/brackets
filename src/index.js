3ddc0f5f23090a1703a89da3ed98f732eb6ae9c4
KononOleg <kononoleg97@gmail.com>
2021-02-22 18:38:38 +0300

    Feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+
+	if(str.length%2!=0)
+	{
+		return false;
+	}
+
+
+	let map = new Map(bracketsConfig);
+	let array = [];
+	
+	for (let i = 0; i < str.length; i++)
+	 {
+
+		if(str[i]==map.get(array[array.length - 1]))
+		{
+			array.pop();
+		}
+		else 
+		{
+			array.push(str[i]);
+		}
+	}
+
+
+	
+	
+	return array.length === 0;
+	}
